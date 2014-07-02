a124e1f8eb336d0d681b0194b64bb3671414baa6
erjiaqing <gs199704@gmail.com>
2014-07-02 14:34:31 +0800

    BZOJ P2209

--- bzoj/2209.cpp
+++ bzoj/2209.cpp
@@ -1,146 +1,192 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn=100005;
-char sq[maxn];
-int cnt;
-int n,q,op,ql,qr;
-int son[maxn][2],lmin[maxn],lmax[maxn],rmin[maxn],rmax[maxn],val[maxn],f[maxn],sum[maxn],sz[maxn],rev[maxn],inv[maxn],root;
-int tn;
-char str[maxn];
-void update(int x)
-{
-	sz[x]=sz[son[x][0]]+sz[son[x][1]]+1;
-	sum[x]=sum[son[x][0]]+sum[son[x][1]]+val[x];
-	lmin[x]=lmin[son[x][0]];
-	lmin[x]=min(lmin[x],sum[son[x][0]]+val[x]);
-	lmin[x]=min(lmin[x],sum[son[x][0]]+val[x]+lmin[son[x][1]]);
-	lmax[x]=lmax[son[x][0]];
-	lmax[x]=max(lmax[x],sum[son[x][0]]+val[x]);
-	lmax[x]=max(lmax[x],sum[son[x][0]]+val[x]+lmax[son[x][1]]);
-	rmin[x]=rmin[son[x][1]];
-	rmin[x]=min(rmin[x],sum[son[x][1]]+val[x]);
-	rmin[x]=min(rmin[x],sum[son[x][1]]+val[x]+rmin[son[x][0]]);
-	rmax[x]=rmax[son[x][1]];
-	rmax[x]=max(rmax[x],sum[son[x][1]]+val[x]);
-	rmax[x]=max(rmax[x],sum[son[x][1]]+val[x]+rmax[son[x][0]]);
-}
-void build(int l,int r)
-{
-	int mid=(l+r)/2;
-	if (l<mid)
-		build(l,mid-1),son[mid][0]=(l+mid-1)/2,f[(l+mid-1)/2]=mid;
-	if (mid<r)
-		build(mid+1,r),son[mid][1]=(mid+r+1)/2,f[(mid+r+1)/2]=mid;
-	update(mid);
-}
-void _rev(int x)
-{
-	rev[x]^=1;
-	swap(son[x][0],son[x][1]);
-	swap(lmin[x],rmin[x]);
-	swap(rmax[x],rmax[x]);
-}
-void _inv(int x)
-{
-	inv[x]^=1;
-	val[x]=-val[x];sum[x]=-sum[x];
-	swap(lmin[x],lmax[x]);swap(rmin[x],rmax[x]);
-	lmin[x]=-lmin[x];lmax[x]=-lmax[x];
-	rmin[x]=-rmin[x];rmax[x]=-rmax[x];
-}
-void push_down(int x)
-{
-	if (rev[x])
+struct node{
+	node *s[2],*f;
+	int lmin,lmax,rmin,rmax;
+	int val,sum,sz;
+	int fo1,fo2;
+	void op2()
 	{
-		if (son[x][0]) _rev(son[x][0]);
-		if (son[x][1]) _rev(son[x][1]);
-		rev[x]=0;
+		fo2^=1;
+		swap(lmin,rmin),swap(lmax,rmax),swap(s[0],s[1]);
 	}
-	if (inv[x])
+	void op1()
 	{
-		if (son[x][0]) _inv(son[x][0]);
-		if (son[x][1]) _inv(son[x][1]);
-		inv[x]=0;
+		fo1^=1;
+		val=-val;sum=-sum;
+		lmin=-lmin,lmax=-lmax,rmin=-rmin,rmax=-rmax;
+		swap(lmin,lmax),swap(rmin,rmax);
+	}
+}*root,pool[maxn],*nullnode;int cnt;
+bool good(node *p)
+{
+	return p!=nullnode && (p!=&pool[1]) && (p!=&pool[2]);
+}
+void push_up(node *p)
+{
+	if (p==nullnode) return;
+	p->sz=p->s[0]->sz+p->s[1]->sz+1;
+	p->sum=p->s[0]->sum+p->s[1]->sum+p->val;
+
+	p->lmin=p->s[0]->lmin;
+	p->lmin=min(p->lmin,p->s[0]->sum+p->val);
+	p->lmin=min(p->lmin,p->s[0]->sum+p->val+p->s[1]->lmin);
+	
+	p->lmax=p->s[0]->lmax;
+	p->lmax=max(p->lmax,p->s[0]->sum+p->val);
+	p->lmax=max(p->lmax,p->s[0]->sum+p->val+p->s[1]->lmax);
+	
+	p->rmin=p->s[1]->rmin;
+	p->rmin=min(p->rmin,p->s[1]->sum+p->val);
+	p->rmin=min(p->rmin,p->s[1]->sum+p->val+p->s[0]->rmin);
+	
+	p->rmax=p->s[1]->rmax;
+	p->rmax=max(p->rmax,p->s[1]->sum+p->val);
+	p->rmax=max(p->rmax,p->s[1]->sum+p->val+p->s[0]->rmax);
+}
+void push_down(node *p)
+{
+	if (p==nullnode) return;
+	if (p->fo1)
+	{
+		if (good(p->s[0])) p->s[0]->op1();
+		if (good(p->s[1])) p->s[1]->op1();
+		p->fo1=0;
+	}
+	if (p->fo2)
+	{
+		if (good(p->s[0])) p->s[0]->op2();
+		if (good(p->s[1])) p->s[1]->op2();
+		p->fo2=0;
 	}
 }
-void rotate(int p,int x)
+node *newnode(int val,node *f)
 {
-	int cx=(son[x][1]==p),y=son[p][cx^1],z=f[x];
-	if (y) f[y]=x;
-	son[z][(x==son[z][1])]=p;
-	son[p][cx^1]=x;f[p]=z;son[x][cx]=y;f[x]=p;
-	update(x);
+	node *ret=&pool[cnt++];
+	ret->s[0]=ret->s[1]=nullnode;
+	ret->f=f;
+	ret->fo1=ret->fo2=0;
+	ret->sz=1;
+	ret->sum=ret->val=ret->lmin=ret->lmax=ret->rmin=ret->rmax=val;
+	return ret;
 }
-void splay(int p,int k)
+void init()
+{
+	nullnode=NULL;
+	nullnode=newnode(0,NULL);
+	nullnode->sz=0;
+	nullnode->lmin=nullnode->rmin=1e6;
+	nullnode->lmax=nullnode->rmax=-1e6;
+	nullnode->sum=0;
+	root=newnode(0,nullnode);
+	root->lmin=root->rmin=1e6;
+	root->lmax=root->rmax=-1e6;
+	root->sum=0;
+	root->s[1]=newnode(0,root);
+	root->s[1]->lmin=root->s[1]->rmin=1e6;
+	root->s[1]->lmax=root->s[1]->rmax=-1e6;
+	root->s[1]->sum=0;
+	push_up(root);
+}
+//--
+void rotate(node *p,node *x)
+{
+	int cx=(p==x->s[1]);
+	node *y=p->s[cx^1],*z=x->f;
+	if (y!=nullnode) y->f=x;
+	if (x==z->s[0]) z->s[0]=p;
+	if (x==z->s[1]) z->s[1]=p;
+	p->s[cx^1]=x;p->f=z;x->s[cx]=y;x->f=p;
+	push_up(x);
+}
+void splay(node *p,node *k)
 {
 	push_down(p);
-	while (f[p]!=k)
+	while (p->f!=k)
 	{
-		int x=f[p],y=f[x];
+		node *x=p->f,*y=x->f;
 		push_down(y);push_down(x);push_down(p);
 		if (y==k)
 			rotate(p,x);
-		else if ((p==son[x][0])^(x==son[y][0]))
+		else if ((p==x->s[0])^(x==y->s[0]))
 			rotate(p,x),rotate(p,y);
 		else
 			rotate(x,y),rotate(p,x);
 	}
-	update(p);
-	if (k==0)
-		root=p;
+	push_up(p);
+	if (k==nullnode) root=p;
 }
-int select(int rank,int k)
+//--
+void select(int rk,node *f)
 {
-	int p=root;
-	while (sz[son[p][0]]+1!=rank)
+	node *p=root;
+	push_down(p);
+	while (p->s[0]->sz+1 != rk)
 	{
-		if (sz[son[p][0]]+1<rank)
+		if (p->s[0]->sz < rk)
 		{
-			rank-=sz[son[p][0]]+1;
-			p=son[p][1];
-		}
-		else
+			rk-=p->s[0]->sz+1;
+			p=p->s[1];
+		}else
 		{
-			p=son[p][0];
+			p=p->s[0];
 		}
 		push_down(p);
 	}
-	splay(p,k);
-	return p;
+	splay(p,f);
 }
-void hehe(int &l,int &r)
+int val[maxn];
+node *build(int l,int r,node *p)
 {
-	l=select(l,0);r=select(r,l);
+	if (l>r) return nullnode;
+	int mid=(l+r)/2;
+	node *ret=newnode(val[mid],p);
+	ret->s[0]=build(l,mid-1,ret);
+	ret->s[1]=build(mid+1,r,ret);
+	push_up(ret);
+	return ret;
 }
-void Rev(int l,int r)
+void insert(int p,int n)
 {
-	hehe(l,r);_rev(son[r][0]);splay(son[r][0],0);
+	select(p+1,nullnode);
+	select(p+2,root);
+	node *r=build(1,n,root->s[1]);
+	root->s[1]->s[0]=r;
+	splay(r,nullnode);
 }
-void Inv(int l,int r)
+void op1(int l,int r)
 {
-	hehe(l,r);_inv(son[r][0]);splay(son[r][0],0);
+	select(l,nullnode),select(r+2,root);
+	root->s[1]->s[0]->op1();
 }
-int Que(int l,int r)
+void op2(int l,int r)
 {
-	return hehe(l,r),(1-lmin[son[r][0]])/2+(1+rmax[son[r][0]])/2;
+	select(l,nullnode),select(r+2,root);
+	root->s[1]->s[0]->op2();
 }
+int query(int l,int r)
+{
+	select(l,nullnode),select(r+2,root);
+	return	-(root->s[1]->s[0]->lmin-1)/2
+			+(root->s[1]->s[0]->rmax+1)/2;
+}
+int n,m;
+char str[maxn];
 int main()
 {
-	scanf("%d%d",&n,&q);
-	scanf("%s",str+2);
-	for (int i=2;i<=n+1;i++) val[i]=(str[i]=='('?1:-1);
-	n+=2;
-	root=(1+n)/2;
-	build(1,n);
-	while (q--)
+	init();
+	scanf("%d%d",&n,&m);
+	scanf("%s",str+1);
+	for (int i=1;i<=n;i++) val[i]=(str[i]=='('?1:-1);
+	insert(0,n);
+	int op,x,y;
+	while (m--)
 	{
-		scanf("%d%d%d",&op,&ql,&qr);
-		if (op==0)
-			printf("%d\n",Que(ql,qr+2));
-		else if (op==1)
-			Inv(ql,qr+2);
-		else if (op==2)
-			Rev(ql,qr+2);
+		scanf("%d%d%d",&op,&x,&y);
+		if (op==0) printf("%d\n",query(x,y));
+		else if (op==1) op1(x,y);
+		else if (op==2) op2(x,y);
 	}
 	return 0;
 }
